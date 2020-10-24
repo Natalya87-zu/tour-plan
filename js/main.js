348e1d8cff01ef514f0873b7a2ec550f30c92a3e
Natalya87-zu <kalipsar87@gmail.com>
2020-10-24 20:23:33 +0500

    create: add modal form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 var mySwiper = new Swiper(".hotel__slider", {
   // Optional parameters
   loop: true,
@@ -28,4 +29,26 @@ menuButton.addEventListener("click", function () {
   document
     .querySelector(" .navbar-bottom")
     .classList.toggle("navbar-bottom--visible");
+});
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal)
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+
+  }
 });
